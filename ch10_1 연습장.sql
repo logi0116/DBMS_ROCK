cea3d0ac4e3da29c160f0e7a7b62a24545f5e251
logi0116 <roguewrite001@gmail.com>
2025-05-22 17:33:54 +0900

    8교시, 기본 insert 예시, 테이블 복사하기, 조회해서 테이블에 입력하기

--- ch10_1 연습장.sql
+++ ch10_1 연습장.sql
@@ -17,21 +17,68 @@
 -- dname: 부서 이름 (예: '개발부2')
 -- loc  : 부서 위치 (예: '서울2')
 
-insert into dept (
-    deptno,
-    dname,
-    loc
-) values (70,'개발부2','서울2');    
+insert into dept ( deptno, dname, loc) values ( 70,'개발부2','서울2' );
 
 
 
 
-select * 
-from dept;
+select *
+  from dept;
+
+insert into dept (deptno,dname, loc) values ( 80,'개발부3','서울3' );
+
 commit;
 
+-- SYSDATE 삽입
 
-1)왜 / 명령어에 독스 안 뜰까
-2)이건 왜 접속이 쉬운 것일까요? 원래는 system 으로 이름하고 맞췄어야 했던
-3)터미널과 워크 스페이스 무슨 차이인지. 
+insert into emp ( empno, ename, hiredate ) values ( 1001, '홍길동', SYSDATE );
+select *  from dept;
 
+
+
+-- 퀴즈 1
+-- dept 테이블에 (99, 'AI팀', 'JEJU') 데이터를 추가하시오.
+insert into dept ( deptno as "유니크", dname, loc) values ( 99,'AI팀','JEJU' ); 
+SELECT * FROM dept;
+
+
+
+
+-- 퀴즈 2
+-- EMP 테이블에 사번 1234, 이름 'LEE', 입사일을 SYSDATE로 추가하시오
+insert into emp (empno, ename, hiredate ) values ( 1234, 'LEE', SYSDATE );
+select *  from emp;   
+
+
+-- 퀴즈 3
+--  DEPT 에 NULL을 포함한 값 삽입
+--테이블에 pk 값 의무적으로 입력하기
+--pk : not null 제약조건이 걸려있음. 그런 열
+
+select * from dept;
+insert into dept ( deptno, dname, loc) values (88,null,null); 
+select * from dept;
+
+
+--데이터 사전에서 테이블의 제약조건 확인해보기 dept 테이블
+select * from user_constraints where table_name = 'DEPT';
+--제약 조건 확인하기
+
+--테이블 복사하는 방법
+-- 새로운 테이블을 생성하는 기본 문법
+-- EMP 테이블에서 부서번호가 30인 사원 데이터를 
+--EMP_TEMP_30 테이블에 복사하기
+
+
+-- EMP_TEMP_30 테이블을 생성. 단순 빈테이블 생성
+create table EMP_TEMP_30 AS SELECT * FROM EMP WHERE 1=0;
+
+--실제 데이터 삽입
+create table EMP_TEMP_40 AS SELECT * FROM EMP WHERE 1=1;
+
+
+insert into EMP_TEMP_30
+select * from EMP where deptno = 30;
+--emp_temp_30테이블 단순 확인
+select * from EMP_TEMP_30;
+select * from EMP_TEMP_40;
